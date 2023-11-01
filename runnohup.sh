99e625732dd9bafafc462f069be2d3e4dda0d6fb
swm623 <swm623@qq.com>
2023-11-01 06:23:41 +0000

    all

--- runnohup.sh
+++ runnohup.sh
@@ -5,4 +5,5 @@ set -e
 
 self_world_size=${1:-"1"}
 self_rank=${2:-"0"}
-CUDA_VISIBLE_DEVICES=${self_rank} nohup  python webdb.py --from_pretrained /ML-A100/sshare-app/saiwanming/models/cogvlm-chat --self_rank ${self_rank} --self_world_size ${self_world_size} --version chat  --english --bf16 > app${self_rank}.log 2>&1 &
+local_rank=${3:-"0"}
+CUDA_VISIBLE_DEVICES=${local_rank} nohup  python webdb.py --from_pretrained /ML-A100/sshare-app/saiwanming/models/cogvlm-chat --self_rank ${self_rank} --self_world_size ${self_world_size} --version chat  --english --bf16 > app${self_rank}.log 2>&1 &
