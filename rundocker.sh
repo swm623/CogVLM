b5d146b907657b3b2204a4dd83fe6fca93b3ec5e
swm623 <swm623@qq.com>
2023-11-02 12:46:24 +0000

    modle path

--- rundocker.sh
+++ rundocker.sh
@@ -6,4 +6,5 @@ torchrun --nnodes=${WORLD_SIZE} --nproc-per-node=2 \
                    --from_pretrained /pfs/sshare/app/saiwanming/models/cogvlm-chat \
                    --target_path  /pfs/sshare/app/saiwanming/data/laion-high-aesthetics_6_recaption \
                    --source_path /pfs/sshare/app/dataset/laion-high-aesthetics_6 \
+                   --local_tokenizer /pfs/sshare/app/saiwanming/models/vicuna-7b-v1.5 \
                    --version chat --english --bf16 --run_mode torchrun
