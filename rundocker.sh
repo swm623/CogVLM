c16539f77c808293cb845fae7b351256d4c4f1b2
swm623 <swm623@qq.com>
2023-11-02 12:13:28 +0000

    rundocker

--- rundocker.sh
+++ rundocker.sh
@@ -4,6 +4,6 @@ torchrun --nnodes=${WORLD_SIZE} --nproc-per-node=8 \
                   --master-port=${MASTER_PORT} \
                    /pfs/sshare/app/saiwanming/CogVLM/webdb.py \
                    --from_pretrained /pfs/sshare/app/saiwanming/models/cogvlm-chat \
-                   --target_path  /pfs/sshare/app/saiwanming/data/laion-high-aesthetics_6_recaption
-                   --source_path /pfs/sshare/app/dataset/laion-high-aesthetics_6
+                   --target_path  /pfs/sshare/app/saiwanming/data/laion-high-aesthetics_6_recaption \
+                   --source_path /pfs/sshare/app/dataset/laion-high-aesthetics_6 \
                    --version chat --english --bf16 --run_mode torchrun
